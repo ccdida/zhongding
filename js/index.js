a21169e83ea2bfb0929e880af84c095000189ed3
ccdida <516727605@qq.com>
2018-03-06 14:52:33 +0800

    commit3

--- js/index.js
+++ js/index.js
@@ -22,3 +22,14 @@ function slideTab(index){
         bullets[index].className = 'on';  
 }
 slideTab(0);
+
+//导航栏点击事件
+
+$(".menu_nav .nav_item1").on('click',function(event){
+   // event.preventDefault();
+   if($('.nav_one').is(':hidden')){//如果当前隐藏  
+        $('.nav_one').show();//那么就显示div  
+        }else{//否则  
+        $('.nav_one').hide();//就隐藏div  
+        }  
+})
